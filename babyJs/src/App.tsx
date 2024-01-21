01a3f2f7ef7f4503401f337a9c1f42f09a0bd88a
ryanquek22 <112756792+ryanquek22@users.noreply.github.com>
2024-01-21 08:59:23 +0800

    added ModalForm component

--- babyJs/src/App.tsx
+++ babyJs/src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { ModalForm } from "./Modal";
 
 function App() {
-
   return (
     <>
-
-     </>
-  )
+      <h1>Hello World</h1>
+      <ModalForm></ModalForm>
+    </>
+  );
 }
 
-export default App
+export default App;
